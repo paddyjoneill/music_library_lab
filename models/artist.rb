76b26ba5243c4b74ae86c9e7dee9b50ba5c59548
James Fraser <jimbob4t@virginmedia.com>
2019-12-04 16:05:01 +0000

    initial commit

---
+++ models/artist.rb
@@ -0,0 +1,33 @@
+require('pg')
+require_relative('../db/sql_runner')
+class Artist
+
+attr_reader :id, :name
+
+  def initialize( options )
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+  end
+
+  def save()
+
+sql = "INSERT INTO artists ( name ) VALUES ( $1 ) RETURNING id"
+values = [@name]
+@id = SqlRunner.run(sql, values)[0]['id'].to_i
+
+end
+def self.all()
+  sql = "SELECT * FROM artists;"
+  artists = SqlRunner.run(sql)
+  return artists.map { | artist | Artist.new(artist)}
+end
+
+
+
+
+
+
+
+
+
+end
